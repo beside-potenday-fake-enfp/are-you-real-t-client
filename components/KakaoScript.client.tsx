5e6e5f0b542733229eaa14c6f9f25c16492eb1fc
2dowon <dwon424@gmail.com>
2024-09-29 19:24:08 +0900

    feat: add kakao share button to home for test

---
+++ components/KakaoScript.client.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Script from "next/script";
+
+const KakaoScript = () => {
+  const onLoad = () => {
+    window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_API_KEY);
+  };
+
+  return (
+    <Script
+      src="https://developers.kakao.com/sdk/js/kakao.js"
+      async
+      onLoad={onLoad}
+      strategy="afterInteractive"
+    />
+  );
+};
+
+export default KakaoScript;
---
+++ utils/constants/url.const.ts
@@ -0,0 +1,1 @@
+export const BASE_URL = "https://are-you-real-t.vercel.app";

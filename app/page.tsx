01fd0ceecdb7e1739d45ef6c2528a4c7821840aa
2dowon <dwon424@gmail.com>
2024-09-29 19:37:20 +0900

    wpi

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import KakaoShareButton from "@/components/KakaoShareButton.client";
 const HomePage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl mb-[1rem] font-bold">카카오톡 공유하기 Test</h1>
+      <h1 className="text-2xl mb-[1rem] font-bold">카카오톡 공유하기 TEST</h1>
       <KakaoShareButton />
     </div>
   );

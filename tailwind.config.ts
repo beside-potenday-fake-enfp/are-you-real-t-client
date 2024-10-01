490ed3377864d03b49fa56f4a798baa04bf7c62c
2dowon <dwon424@gmail.com>
2024-10-01 15:27:24 +0900

    setting: css, tailwind style setting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,20 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "gray-0": "#000000",
+        "gray-10": "#191919",
+        "gray-25": "#404040",
+        "gray-60": "#93979B",
+        "gray-90": "#E5E5E5",
+        "gray-100": "#FFFFFF",
+        primary: "#FF53AB",
+        "primary-active": "#FF1E91",
+        "primary-disabled": "#B53577",
+      },
+      fontSize: {
+        h1: ["3.4rem", { fontWeight: "600", letterSpacing: "-2%" }],
+        h2: ["2rem", { fontWeight: "600", letterSpacing: "-2%" }],
+        body1: ["1.3rem", { fontWeight: "400", letterSpacing: "-2%" }],
       },
     },
   },

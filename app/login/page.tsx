6d35bd6c163b5ebfbcbeaee811b09189f1c05f1a
2dowon <dwon424@gmail.com>
2024-10-03 11:40:24 +0900

    chore: small updates

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,11 @@
-import Caret from "@/components/icon/Caret";
-import Link from "next/link";
 import { Suspense } from "react";
 import TesterLoginPage from "./TesterLoginPage.client";
 
 const LoginPage = () => {
   return (
     <div>
-      <div className="mx-[2.6rem] mt-[3.2rem] mb-[4.8rem]">
-        <Link href="/" className="flex items-center gap-x-[1rem]">
-          <Caret className="w-[1.8rem] h-[1.8rem]" />
-          <p className="text-title text-white">나의 MBTI는?</p>
-        </Link>
+      <div className="px-[2rem] my-[3.2rem]">
+        <p className="text-detail text-white">나의 MBTI는?</p>
       </div>
 
       <Suspense>
